2b67898fe9cfad1909900fdf925aa0a8b1ff8a6c
WESAMMOHAMMED <wesamalmoqry@gmail.com>
2023-10-19 02:26:38 +0300

    add logs

--- jwt/Seeder/AccountsSeeder.cs
+++ jwt/Seeder/AccountsSeeder.cs
@@ -3,20 +3,22 @@
     public class AccountsSeeder : IDataSeeder
     {
         private readonly ApplicationDbContext _dbContext;
-        
+        private readonly ILogger<AccountsSeeder> _logger;
 
-        public AccountsSeeder(ApplicationDbContext dbContext)
+        public AccountsSeeder(ApplicationDbContext dbContext, ILogger<AccountsSeeder> logger)
         {
             this._dbContext = dbContext;
-            
+            _logger = logger;
 
 
         }
         public async Task SeedDataAsync()
         {
+            _logger.LogInformation($"########Start seeding#########");
             var accouns = await _dbContext.Set<Account>().ToListAsync();
             if(accouns.Any())
             {
+                _logger.LogInformation($"######## {nameof(accouns)}: #########",accouns);
                 return;
             }
             var newAccounts =new List<Account>(){
@@ -26,7 +28,9 @@
                 new Account { Name = "المصروفات", Id = 4, AccountNumber = 4, AccountType = AccountType.Other, AppearIn = AppearIn.INCOME },
 
             };
+            _logger.LogInformation($"######## {nameof(newAccounts)}: #########", newAccounts);
             await _dbContext.Set<Account>().AddRangeAsync(newAccounts);
+            await _dbContext.SaveChangesAsync();
             
         }
     }
